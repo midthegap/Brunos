6208a786fdb7c035ea733ddf5f080d1e975b6bfe
Stefano Peruzzi <stefano.peruzzi@magentalab.it>
2025-05-23 14:44:21 +0200

    Move style to css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { BrunoListComponent } from "./bruno-list/bruno-list.component";
 import { MenuComponent } from "./menu/menu.component";
 
